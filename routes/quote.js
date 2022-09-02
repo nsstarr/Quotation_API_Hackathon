4d9ec562e22979390de5b29b57618d1a61c1261d
ArianMoossavi <arianmoossavi@live.co.uk>
2022-09-02 13:29:46 +0100

    logic for age, breed and region implemented

--- routes/quote.js
+++ routes/quote.js
@@ -2,24 +2,50 @@ import express from "express";
 
 const router = express.Router();
 
+function ageMultiplier(price, age) {
+  if (age < 6) {
+    price = ((age * 5 + 100) / 100) * price;
+  } else if (age < 10) {
+    price = (((age % 5) * 10 + 125) / 100) * price;
+  } else {
+    price = price * 1.75;
+  }
+  return price;
+}
+
+function breedDiscount(price, breed) {
+  if (
+    breed === "Jack Russell" ||
+    breed === "Chihuahua" ||
+    breed === "Yorkshire Terrier"
+  ) {
+    price = price * 0.9;
+  }
+  return price;
+}
+
+function regionMultiplier(price, region) {
+  if (region === "London" || region === "SouthEast" || region === "East") {
+    price = price * 1.15;
+  }
+  return price;
+}
+
 router.get("/", async (req, res) => {
   res.send("you are at the quote router get");
 });
 
 router.post("/", async (req, res) => {
   // const {age, species, breed, address} = req.body;
-  const {age} = req.body
+  const petNumber = req.body.length;
+  const payload = {};
+  const { age, breed, region } = req.body[0];
   let price = 120;
-  if (age < 6) {
-    price = (((age * 5) + 100) / 100) * price
-  }
-  else if (age < 10) {
-    price = ((((age % 5) * 10) + 125) / 100) * price 
-  }
-  else {
-    price = price * 1.75
-  }
-  console.log(price)
-})
+  price = ageMultiplier(price, age);
+  price = breedDiscount(price, breed);
+  price = regionMultiplier(price, region);
+
+  res.json({ price: Math.round(price * 100) / 100 });
+});
 
 export default router;
