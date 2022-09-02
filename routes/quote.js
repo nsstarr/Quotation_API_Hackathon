5f7c68daf503ea8d2a4f02544ac2bbc7c02ce905
JackC91 <jackjcherry@hotmail.co.uk>
2022-09-02 12:00:19 +0100

    post req written and number functionality working and req working

--- routes/quote.js
+++ routes/quote.js
@@ -6,4 +6,20 @@ router.get("/", async (req, res) => {
   res.send("you are at the quote router get");
 });
 
+router.post("/", async (req, res) => {
+  // const {age, species, breed, address} = req.body;
+  const {age} = req.body
+  let price = 120;
+  if (age < 6) {
+    price = (((age * 5) + 100) / 100) * price
+  }
+  else if (age < 10) {
+    price = ((((age % 5) * 10) + 125) / 100) * price 
+  }
+  else {
+    price = price * 1.75
+  }
+  console.log(price)
+})
+
 export default router;
