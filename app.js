735116cde6e364a40f2f090991e3537373a816c8
skandog <muumrar@gmail.com>
2022-09-02 14:24:48 +0100

    created request counter on a dash route

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import quoteRouter from "./routes/quote.js";
+// import dashRouter from "./routes/dash.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +12,24 @@ app.get("/", (req, res) => {
   res.json({ message: "hello from server" });
 });
 
+let page_visits = {};
+let visits = function (req, res, next) {
+  let counter = page_visits[req.originalUrl];
+  if (counter || counter === 0) {
+    page_visits[req.originalUrl] = counter + 1;
+  } else {
+    page_visits[req.originalUrl] = 1;
+  }
+  console.log(req.originalUrl, counter);
+  next();
+};
+
+app.use(visits);
+
 app.use("/quote", quoteRouter);
+app.get("/dash", (req, res) => {
+  res.json(page_visits);
+});
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
